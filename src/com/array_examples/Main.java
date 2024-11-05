bef4f30033c24aa98ade9fbc9f450e98296ef350
everviera12 <evertigre10@gmail.com>
2024-11-05 13:05:48 -0600

    commit

--- src/com/array_examples/Main.java
+++ src/com/array_examples/Main.java
@@ -2,7 +2,6 @@ package com.array_examples;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] array = new int[10];
         int[] array = new int[10];
 
         // for with numbers
